583f4b1308700cb31ee5d44101d7ade214ec26bb
Dmytro_Marchenko <dmmmarchenko@corelogic.com>
2021-12-04 18:19:54 +0200

    Day 4

---
+++ src/test/java/Day04Test.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day04Test {
+
+    @Test
+    internal fun part1Simple() {
+        val actualResult = Day04.part1(readInput("day4Simple"))
+        assertEquals(4512, actualResult)
+    }
+
+    @Test
+    internal fun part1Real() {
+        val actualResult = Day04.part1(readInput("day4Real"))
+        assertEquals(39984, actualResult)
+    }
+
+    @Test
+    internal fun part2Simple() {
+        val actualResult = Day04.part2(readInput("day4Simple"))
+        assertEquals(1924, actualResult)
+    }
+
+    @Test
+    internal fun part2Real() {
+        val actualResult = Day04.part2(readInput("day4Real"))
+        assertEquals(8468, actualResult)
+    }
+}
