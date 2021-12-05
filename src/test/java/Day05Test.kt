4acf19bc96f130c40a3ee14468eeee3fdfb3c9b0
Dmytro Marchenko <ace.vendigo@gmail.com>
2021-12-05 19:30:37 +0200

    Day 5

---
+++ src/test/java/Day05Test.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+
+    @Test
+    internal fun part1Simple() {
+        val actualResult = Day05.part1(readInput("day5Simple"))
+        assertEquals(5, actualResult)
+    }
+
+    @Test
+    internal fun part1Real() {
+        val actualResult = Day05.part1(readInput("day5Real"))
+        assertEquals(6225, actualResult)
+    }
+
+    @Test
+    internal fun part2Simple() {
+        val actualResult = Day05.part2(readInput("day5Simple"))
+        assertEquals(12, actualResult)
+    }
+
+    @Test
+    internal fun part2Real() {
+        val actualResult = Day05.part2(readInput("day5Real"))
+        assertEquals(0, actualResult)
+    }
+}
