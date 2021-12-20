9878ae586997d6bd4ef48eeb4196cb2e8f9378c3
Dmytro_Marchenko <dmmmarchenko@corelogic.com>
2021-12-20 13:36:30 +0200

    Day 18

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -1,11 +1,31 @@
+import kotlin.math.max
+
 object Day18 {
 
-    fun part1(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Long {
+        var sum = parseNumber(input.first())
+
+        input.drop(1).forEach {
+            val next = parseNumber(it)
+            sum = sum.plus(next)
+        }
+
+        return sum.magnitude()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var maxMagnitude = 0L
+
+        for (i in input.indices) {
+            for (j in input.indices) {
+                if (i != j) {
+                    val magnitude = parseNumber(input[i]).plus(parseNumber(input[j])).magnitude()
+                    maxMagnitude = max(maxMagnitude, magnitude)
+                }
+            }
+        }
+
+        return maxMagnitude
     }
 
     fun parseNumber(str: String): Node {
@@ -49,46 +69,145 @@ object Day18 {
             return 3 * left!!.magnitude() + 2 * right!!.magnitude()
         }
 
-        fun traverse() {
-            traverse(0, this)
+        fun plus(other: Node): Node {
+            val newRoot = Node(null)
+            newRoot.left = this
+            this.parent = newRoot
+            newRoot.right = other
+            other.parent = newRoot
+            newRoot.reduce()
+            return newRoot
         }
 
-        fun explode() {
-            traverse(0, this)
+        fun reduce() {
+            var reducable: Boolean
+            do {
+                reducable = if (explode()) {
+                    true
+                } else {
+                    split()
+                }
+            } while (reducable)
+        }
+
+        fun explode(): Boolean {
+            val context = TraverseContext()
+            traverse(0, this, context)
+            return context.command != CommandType.EXPLODE
+        }
+
+        fun split(): Boolean {
+            val context = TraverseContext(command = CommandType.SPLIT)
+            traverse(0, this, context)
+            return context.command != CommandType.SPLIT
+        }
+
+        fun asString(): String {
+            val str = StringBuilder()
+            traversePrint(0, this, str)
+            return str.toString()
+        }
+
+        override fun toString(): String {
+            return if (this.value != null) this.value.toString() else "null"
         }
     }
 
-    fun traverse(level: Int, node: Node?) {
-        if (node == null) {
+    private fun traversePrint(level: Int, node: Node, str: StringBuilder) {
+        if (node.value != null) {
+            str.append(node.value)
+        }
+
+        if (node.left != null) {
+            str.append("[")
+            traversePrint(level + 1, node.left!!, str)
+            str.append(",")
+        }
+
+        if (node.right != null) {
+            traversePrint(level + 1, node.right!!, str)
+            str.append("]")
+        }
+    }
+
+    private fun traverse(level: Int, node: Node?, context: TraverseContext) {
+        if (node == null || context.command == CommandType.EXIT) {
             return
         }
 
-        println("$level) ${node.value}")
+        if (node.value != null) {
+            context.visitedNodes += node
 
-        val leftValue = node.left?.value
-        val rightValue = node.right?.value
+            if (context.command == CommandType.INC_RIGHT) {
+                addToRight(node, context)
+                return
+            }
 
-        if (level == 3 && leftValue != null && rightValue != null) {
-            addToLeft(node, leftValue)
-            addToRight(node, rightValue)
-            node.left = null
-            node.right = null
-            node.value = 0
+            if (context.command == CommandType.SPLIT && node.value!! > 9) {
+                split(node, context)
+                return
+            }
         }
 
-        traverse(level + 1, node.left)
-        traverse(level + 1, node.right)
+        if (context.command == CommandType.EXPLODE) {
+            val leftValue = node.left?.value
+            val rightValue = node.right?.value
+
+            if (level >= 4 && leftValue != null && rightValue != null) {
+                explode(leftValue, rightValue, context, node)
+                return
+            }
+        }
+
+        traverse(level + 1, node.left, context)
+        traverse(level + 1, node.right, context)
     }
 
-    fun addToLeft(node: Node, v: Long) {
-        var prev = null
-        var n = node
+    private fun split(node: Node, context: TraverseContext) {
+        val half = node.value!! / 2.0
+        node.value = null
+        val leftNode = Node(node, half.toLong())
+        leftNode.parent = node
+        node.left = leftNode
+        val rightNode = Node(node, half.roundUp())
+        rightNode.parent = node
+        node.right = rightNode
+        context.command = CommandType.EXIT
+    }
 
-        while (n.parent != null && n == n.parent) {
+    private fun Double.roundUp(): Long {
+        val asLong = this.toLong()
+        return if (this == asLong.toDouble()) asLong else asLong + 1
+    }
 
+    private fun explode(leftValue: Long, rightValue: Long, context: TraverseContext, node: Node) {
+        addToLeft(leftValue, context.visitedNodes)
+        node.left = null
+        node.right = null
+        node.value = 0
+        context.command = CommandType.INC_RIGHT
+        context.incValue = rightValue
+    }
+
+    private fun addToLeft(v: Long, nodesVisited: List<Node>) {
+        if (nodesVisited.isNotEmpty()) {
+            val leftItem = nodesVisited.last()
+            leftItem.value = leftItem.value!! + v
         }
     }
 
-    fun addToRight(node: Node, v: Long) {
+    private fun addToRight(node: Node, context: TraverseContext) {
+        node.value = node.value!! + context.incValue!!
+        context.command = CommandType.EXIT
+    }
+
+    data class TraverseContext(
+        var command: CommandType = CommandType.EXPLODE,
+        var incValue: Long? = null,
+        val visitedNodes: MutableList<Node> = mutableListOf()
+    )
+
+    enum class CommandType {
+        EXPLODE, INC_RIGHT, EXIT, SPLIT
     }
 }
--- src/test/java/Day18Test.kt
+++ src/test/java/Day18Test.kt
@@ -26,10 +26,49 @@ class Day18Test {
         assertEquals(expectedMagnitude, number.magnitude())
     }
 
+    fun explodeArgs(): List<Arguments> {
+        return listOf(
+            Arguments.of("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
+            Arguments.of("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+            Arguments.of("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"),
+            Arguments.of("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+            Arguments.of("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("explodeArgs")
+    fun explode(input: String, expectedExploded: String) {
+        val number = Day18.parseNumber(input)
+        val explode = number.explode()
+        assertEquals(true, explode)
+        assertEquals(expectedExploded, number.asString())
+    }
+
     @Test
-    fun traverse() {
-        val number = Day18.parseNumber("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
-        number.traverse()
+    fun noExplode() {
+        val input = "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+        val number = Day18.parseNumber(input)
+        val explode = number.explode()
+        assertEquals(false, explode)
+        assertEquals(input, number.asString())
+    }
+
+    @Test
+    fun split() {
+        val input = "[[[[0,7],4],[15,[0,13]]],[1,1]]"
+        val number = Day18.parseNumber(input)
+        val split = number.split()
+        assertEquals(true, split)
+        assertEquals("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", number.asString())
+    }
+
+    @Test
+    fun plus() {
+        val a = Day18.parseNumber("[[[[4,3],4],4],[7,[[8,4],9]]]")
+        val b = Day18.parseNumber("[1,1]")
+        val sum = a.plus(b)
+        assertEquals("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", sum.asString())
     }
 
     @Test
@@ -41,13 +80,13 @@ class Day18Test {
     @Test
     fun part1Real() {
         val actualResult = Day18.part1(readInput("day18Real"))
-        assertEquals(0, actualResult)
+        assertEquals(4289, actualResult)
     }
 
     @Test
     fun part2Simple() {
         val actualResult = Day18.part2(readInput("day18Simple"))
-        assertEquals(0, actualResult)
+        assertEquals(3993, actualResult)
     }
 
     @Test
