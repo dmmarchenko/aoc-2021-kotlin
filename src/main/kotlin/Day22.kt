4559c8b170d84d1e07d71d3b1585380db165b112
Dmytro Marchenko <ace.vendigo@gmail.com>
2021-12-26 01:20:10 +0200

    Day 22 part 2

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -2,17 +2,43 @@ object Day22 {
 
     fun part1(input: List<String>): Int {
         val commands = parseInput(input)
-        val min = commands.map { it.cuboid }
-            .minOf { it.min() }
-        val max = commands.map { it.cuboid }
-            .maxOf { it.max() }
-        println("$min - $max")
+        val lightPoints = mutableSetOf<Point>()
 
-        return 0
+        commands
+            .filter { it.cuboid.inBounds() }
+            .forEach {
+                val points = it.cuboid.points()
+                if (it.on) {
+                    lightPoints += points
+                } else {
+                    lightPoints -= points
+                }
+            }
+
+        return lightPoints.size
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val commands = parseInput(input)
+        val cuboids = Cuboids()
+
+        commands.forEach { (on, cuboid) ->
+            if (on) {
+                cuboids.plus(cuboid)
+            } else {
+                cuboids.minus(cuboid)
+            }
+        }
+
+        for (c in cuboids.cuboids) {
+            for (cc in cuboids.cuboids) {
+                if (c != cc && c.isIntersect(cc)) {
+                    println("$c <> $cc")
+                }
+            }
+        }
+
+        return cuboids.pointsNumber()
     }
 
     private fun parseInput(input: List<String>): List<Command> {
@@ -32,19 +58,128 @@ object Day22 {
 
     private fun List<Int>.toIntRange(): IntRange = this[0]..this[1]
 
-    private class Command(val on: Boolean, val cuboid: Cuboid)
+    private data class Point(val x: Int, val y: Int, val z: Int)
 
-    private class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange) {
-        fun min(): Int {
-            return listOf(x, y, z)
-                .map { it.first }
-                .minOf { it }
+    private data class Command(val on: Boolean, val cuboid: Cuboid)
+
+    private class Cuboids {
+        val cuboids = mutableListOf<Cuboid>()
+
+        fun plus(cuboid: Cuboid) {
+            val intersects = cuboids.filter { it.isIntersect(cuboid) }
+            if (intersects.isEmpty()) {
+                cuboids += cuboid
+                return
+            }
+
+            val firstPlus = intersects.first().plus(cuboid)
+            if (intersects.size == 1) {
+                cuboids += firstPlus
+                return
+            }
+
+            firstPlus.forEach {
+                plus(it)
+            }
         }
 
-        fun max(): Int {
-            return listOf(x, y, z)
-                .map { it.last }
-                .maxOf { it }
+        fun minus(cuboid: Cuboid) {
+            val intersects = cuboids.filter { it.isIntersect(cuboid) }
+            if (intersects.isNotEmpty()) {
+                intersects.forEach { intersected ->
+                    cuboids -= intersected
+                    cuboids += intersected.minus(cuboid)
+                }
+            }
         }
+
+        fun pointsNumber(): Long {
+            return cuboids.sumOf { it.pointsNumber() }
+        }
+
+        fun points(): Set<Point> {
+            return cuboids.flatMap { it.points() }.toSet()
+        }
+    }
+
+    private data class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange) {
+
+        fun isIntersect(other: Cuboid): Boolean {
+            return x.isIntersect(other.x) &&
+                    y.isIntersect(other.y) &&
+                    y.isIntersect(other.y)
+        }
+
+        fun plus(other: Cuboid): List<Cuboid> {
+            return join(this, other)
+        }
+
+        fun minus(other: Cuboid): List<Cuboid> {
+            return join(other, this)
+        }
+
+        fun join(first: Cuboid, second: Cuboid): List<Cuboid> {
+            val (_, intX, onlyX) = first.x.intersect(second.x)
+            val (_, intY, onlyY) = first.y.intersect(second.y)
+            val (_, intZ, onlyZ) = first.z.intersect(second.z)
+
+            return listOf(
+                Cuboid(onlyX, intY, intZ),
+                Cuboid(onlyX, intY, onlyZ),
+                Cuboid(intX, onlyY, intZ),
+                Cuboid(intX, onlyY, onlyZ),
+                Cuboid(onlyX, onlyY, intZ),
+                Cuboid(onlyX, onlyY, onlyZ),
+                Cuboid(intX, intY, onlyZ),
+            )
+                .filter { !it.isEmpty() }
+        }
+
+        fun points(): Set<Point> {
+            return x.flatMap { xC ->
+                y.flatMap { yC ->
+                    z.map { zC ->
+                        Point(xC, yC, zC)
+                    }
+                }
+            }.toSet()
+        }
+
+        fun inBounds(): Boolean {
+            return listOf(x.first, y.first, z.first).all { it >= -50 } &&
+                    listOf(x.last, y.last, z.last).all { it <= 50 }
+        }
+
+        fun pointsNumber(): Long {
+            return x.length() * y.length() * z.length()
+        }
+
+        private fun isEmpty(): Boolean {
+            return x.isEmpty() || y.isEmpty() || z.isEmpty()
+        }
+
+        override fun toString(): String {
+            return listOf(x.toString(), y.toString(), z.toString()).joinToString()
+        }
+    }
+
+    private fun IntRange.isIntersect(other: IntRange): Boolean {
+        return this.first <= other.first && this.last >= other.first
+                || other.first <= this.first && other.last >= this.first
+    }
+
+    private fun IntRange.intersect(other: IntRange): List<IntRange> {
+        val first = if (this.first <= other.first) this else other
+        val second = if (this.first <= other.first) other else this
+
+        return listOf(
+            first.first until second.first,
+            second.first..first.last,
+            first.last + 1..second.last
+        )
+    }
+
+    private fun IntRange.length(): Long {
+        return this.last.toLong() - this.first + 1
     }
 }
